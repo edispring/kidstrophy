2a2f7dd9eb1bcf929e33ef1c0c14712a31ef10e5
Edi Spring <edi.spring@lambda-it.ch>
2017-05-01 22:17:11 +0200

    upsert

--- routes/create-run.ts
+++ routes/create-run.ts
@@ -1,5 +1,6 @@
 import * as mongoose from 'mongoose';
 import * as Promise from 'bluebird';
+import * as _ from 'lodash';
 
 import '../models/kid';
 
@@ -11,18 +12,40 @@ export function createRun(req, res, next) {
     .then(() => {
       console.log('create-run', req.body);
       const result = req.body
-      const kid: any = new Kid(result);
-      kid.runs.push({ stage1: 2.2, stage2: 3 });
+      const kid: any = { firstName: result.firstName, lastName: result.lastName, category: +result.category, runs: [] };
 
-      return kid.save();
+      const runs = _.keysIn(result).reduce((p, c, i) => {
+        const [stage, run] = c.split('-');
+        if (run === 'run1')
+          p[0][stage] = +result[c];
+        else if (run === 'run2')
+          p[1][stage] = +result[c];
+
+        return p;
+      }, [{}, {}]);
+
+      if (!_.isEmpty(runs[0])) {
+        runs[0].number = 1;
+        kid.runs.push(runs[0]);
+      }
+
+      if (!_.isEmpty(runs[1])) {
+        runs[1].number = 2;
+        kid.runs.push(runs[1]);
+      }
+
+      return kid;
+    })
+    .then(kid => {
+      return Kid.findOneAndUpdate({ firstName: kid.firstName, lastName: kid.lastName, category: +kid.category }, kid, { upsert: true, new: true, setDefaultsOnInsert: true }).exec();
     })
     .then((result) => {
-      console.log('create-run save result', result);
+      //console.log('create-run save result', result);
       res.redirect('/');
     })
     .catch((e) => {
-      console.error('create-run',e);
-      res.render('index', { title: 'Kidstrophy 2017', error: e.message });
+      console.error('create-run', e);
+      res.render('index', { title: 'Kidstrophy 2017', error: '' + e });
     })
     .finally(() => {
       //next();
