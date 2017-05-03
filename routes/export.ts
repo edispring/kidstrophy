2826de3f745205f848483576b2ca73114e5a8e4e
Edi Spring <edi.spring@lambda-it.ch>
2017-05-03 20:10:04 +0200

    better export

--- routes/export.ts
+++ routes/export.ts
@@ -3,12 +3,13 @@ import * as Promise from 'bluebird';
 import * as _ from 'lodash';
 import * as json2csv from 'json2csv';
 
-const fields = ['id', 'firstName', 'lastName', 'category', 'year', 'stage1', 'stage2', 'stage3', 'stage4', 'stage5', 'stage6'];
+const fields = ['id', 'firstName', 'lastName', 'category', 'year', 'stage1', 'stage2', 'stage3', 'stage4', 'stage5', 'stage6', 'points1', 'points2', 'points3', 'points4', 'points5', 'points6', 'points'];
 
 const Kid = mongoose.model('Kid');
 
 import '../models/kid';
 import { Category, currentYear, categoryValues } from '../config';
+import { calculateRanks } from './ranking';
 
 export function rankingExport(req, res, next) {
     return Promise.resolve()
@@ -16,36 +17,9 @@ export function rankingExport(req, res, next) {
         .then(list => {
             //console.log('list', list);
 
-            const ranks = list.reduce((p, c, i) => {
-
-                const currentRuns = c.runs.filter(x => x.year === currentYear)
-
-                const runCount = currentRuns.length;
-                if (runCount < 1) return p;
-
-                const rank: any = {};
-                p.push(rank);
-
-                rank.id = c._id;
-                rank.firstName = c.firstName;
-                rank.lastName = c.lastName;
-                rank.category = Category[c.category];
-                rank.year = currentYear;
-
-                rank.stage1 = _.minBy(currentRuns.filter(x => x.stage1 > 0), 'stage1').stage1 || 1000;
-                rank.stage2 = _.maxBy(currentRuns.filter(x => x.stage2 > 0), 'stage2').stage2;
-                rank.stage3 = _.minBy(currentRuns.filter(x => x.stage3 > 0), 'stage3').stage3 || 1000;
-                rank.stage4 = _.maxBy(currentRuns.filter(x => x.stage4 > 0), 'stage4').stage4;
-                rank.stage5 = _.minBy(currentRuns.filter(x => x.stage5 > 0), 'stage5').stage5 || 1000;
-                rank.stage6 = _.minBy(currentRuns.filter(x => x.stage6 > 0), 'stage6').stage6 || 1000;
-
-                return p;
-            }, []);
-
-            return ranks;
-        })
-        .then(ranks => {
-            var result = json2csv({ data: ranks, fields: fields });
+            
+            const resultsPerCategory = calculateRanks(list);
+            var result = json2csv({ data: _.flatten(resultsPerCategory), fields: fields });
             res.set('Content-Type', 'text/csv');
             res.set('Content-Disposition', 'attachment; filename="export.csv"');
             res.status(200).send(result);
