6a199ae61b6b7cb0f3d11d972d5430bb11b5590f
Edi Spring <edi.spring@lambda-it.ch>
2022-05-05 20:17:39 +0200

    netifly

--- index.ts
+++ index.ts
@@ -1,22 +1,3 @@
-import * as express from "express";
-import * as path from "path";
-import * as favicon from "serve-favicon";
-import * as logger from "morgan";
-import * as cookieParser from "cookie-parser";
-import * as bodyParser from "body-parser";
-import * as http from "http";
-import * as Promise from "bluebird";
-
-import mongoose = require("mongoose");
-
-import { router } from "./router";
-import { dbConnection } from "./config";
-
-export const app = express();
-
-mongoose.connect(dbConnection).then(console.log).catch(console.error);
-mongoose.Promise = Promise;
-
 const debug = require("debug")("kidstrophy:server");
 
 /**
@@ -24,54 +5,11 @@ const debug = require("debug")("kidstrophy:server");
  */
 
 const port = normalizePort(process.env.PORT || "3000");
+const app = require("./app");
 app.set("port", port);
 
-/**
- * Create HTTP server.
- */
-
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, "public")));
-
-router(app);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  const err: any = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+app.listen(port);
+app.on("error", onError);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -117,14 +55,4 @@ function onError(error) {
     default:
       throw error;
   }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-  debug("Listening on " + bind);
 }
